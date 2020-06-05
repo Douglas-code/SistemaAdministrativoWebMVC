14556608182037f52a1775c3b936cd86bba745a5
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2020-06-05 16:22:22 -0300

    Atualizando layout e mudando tema do bootstrap

--- SistemaAdministrativoWebMvc/Controllers/DepartamentosController.cs
+++ SistemaAdministrativoWebMvc/Controllers/DepartamentosController.cs
@@ -1,14 +1,24 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using SistemaAdministrativoWebMvc.Models;
+using SistemaAdministrativoWebMvc.Models.Data;
+using System.Linq;
 
 namespace SistemaAdministrativoWebMvc.Controllers
 {
     public class DepartamentosController : Controller
     {
+        private readonly SisAdminMvcContext _context;
+
+        public DepartamentosController(SisAdminMvcContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var departamentos = _context.Departamento.Where(d => d.Id > 0).ToList();
+            
+            return View(departamentos);
         }
     }
 }
