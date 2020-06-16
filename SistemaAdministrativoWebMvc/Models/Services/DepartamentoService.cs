7b922b97d25f6b0309e949a004422ffa9db678a0
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2020-06-16 14:38:27 -0300

    Adicionando DepartamentoService

---
+++ SistemaAdministrativoWebMvc/Models/Services/DepartamentoService.cs
@@ -0,0 +1,27 @@
+using SistemaAdministrativoWebMvc.Models.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaAdministrativoWebMvc.Models.Services
+{
+    public class DepartamentoService
+    {
+        private readonly SisAdminMvcContext _context;
+
+        public DepartamentoService(SisAdminMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departamento> ListarDepartamentos()
+        {
+            return _context.Departamento.ToList();
+        }
+
+        public void Inserir(Departamento obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
